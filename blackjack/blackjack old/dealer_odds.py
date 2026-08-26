4063dfeaecdf5d21578f3609a111ebf5ad5ed185
SimpleCrawdad42 <willmorlock7@gmail.com>
2026-08-26 17:33:32 +0000

    Add files via upload

---
+++ blackjack/blackjack old/dealer_odds.py
@@ -0,0 +1,18 @@
+from evcalc import dealer
+
+ev = dealer()
+
+for i, card in enumerate(ev.order):
+    ev.dlrnum(card)
+    print (f'''\n\n
+    If the dealer shows a {card}, the dealer outcomes are as follows:
+    a {round((ev.totals[0] * 100), 1)}% chance of 17
+    a {round((ev.totals[1] * 100), 1)}% chance of 18
+    a {round((ev.totals[2] * 100), 1)}% chance of 19
+    a {round((ev.totals[3] * 100), 1)}% chance of 20
+    a {round((ev.totals[4] * 100), 1)}% chance of 21
+    a {round((ev.totals[5] * 100), 1)}% chance of busting
+    a {round((ev.totals[6] * 100), 1)}% chance of blackjack\n\n
+    ''')
+
+print('** note that infinite deck is assumed in this simulation **\n')
--- blackjack/game.py
+++ blackjack/game.py
@@ -1,202 +1,194 @@
-from shoe import shoe, stack
+from gameclasses import *
 
-while True:
-    try:
-        d = int(input('How many decks are you playing?'))
-        break
-    except ValueError:
-        print("Please enter a valid number")
+# while True:
+#     try:
+#         d = int(input('How many decks are you playing?'))
+#         break
+#     except ValueError:
+#         print("Please enter a valid number")
 
-while True:
-    try:
-        ss = float(input('How many chips are you buying?'))
-        break
-    except ValueError:
-        print("Please enter a fraction")
+# while True:
+#     try:
+#         ss = float(input('How many chips are you buying?'))
+#         break
+#     except ValueError:
+#         print("Please enter a valid number")
 
-stack = stack(ss)
-shoe1 = shoe(d)
-shoe1.shuffle()
+# while True:
+#     try:
+#         pen = float(input('What is the deck penetration (as a decimal)?'))
+#         break
+#     except ValueError:   
+#         print("Please enter a valid number")
+
+# while True:
+#     h17 = input('Dealer hits on soft 17? (y/n)')
+#     if h17.lower() == 'y':
+#         h17 = True
+#         break
+#     elif h17.lower() == 'n':
+#         h17 = False
+#         break
+#     else:
+#         print("Please enter y or n")
+
+d = 6
+ss = 200
+pen = .25
+r = 1.5
+
+h17 = True
+das = True
+spa = True
+rsa = True
+maxsplits = None
+
+
+shoe = Shoe(d)
+stack = Stack(ss)
+action = Action(shoe)
+dealer = Dealer()
+player = Player(stack = stack, action = action)
+
+game = Game(dealer = dealer, player = player, action = action, stack = stack)
 
 while 1==1:
 
-    ############################################################
-    # INITIAL DEAL AND BET
-    ############################################################
-    shoe1.deal()
-    print(f"\n\nNEW DEAL\n\n")
-    print(f'You have {stack.size} chips')
+    player.acereset()
+    action.reset()
 
-    while True:
-        try:
-            bet = float(input('What is your bet?'))
-            break
-        except ValueError:
-            print("Please enter a number")
+    print(f'Your stack size is {stack.size}')
 
-    stack.entbet(bet)
+    # while True:
+    #     try:
+    #         bet = float(input('Please enter a bet'))
+    #         break
+    #     except ValueError:
+    #         print("Please enter a valid number")
 
-
+    bet = 5
+    stack.bet(bet)
     print(f'You bet {bet} chips')
 
-    print(f'You Are Dealt {shoe1.cards}')
-    print(f'Dealer Shows {shoe1.dealerup}')
+    action.deal()
+    print(f'You Are Dealt {action.cards[0]}')
+    print(f'Dealer Shows {action.dealerhand[0]}')
+    
 
-    ############################################################
-    # CHECK FOR BLACKJACKS
-    ############################################################
 
-    shoe1.bjcheck()
+    bjc = game.blackjackcheck(r)
+    if bjc == 'lose':
+        print('Dealer Blackjack, skill issue')
+    elif bjc == 'push':
+        print('Two Blackjacks, push')
+    elif bjc == 'win':
+        print('Blackjack, nice hand!')
 
-    if shoe1.playerbj and shoe1.dealerbj:
-        print('Two Blackjacks, Push')
-        stack.push()
-        continue
 
-    elif shoe1.playerbj:
-        print('Blackjack!')
-        stack.blackjack(1.5)
-        continue
-
-    elif shoe1.dealerbj:
-        print(f'Dealer Shows {shoe1.dealerdown}')
-        print('FUCK YOU')
-        stack.lose()
-        continue
-
-    else:
-        print('No Blackjack')
-
-    ############################################################
-    # CHECK IF SPLITS ARE AVAILABLE // RESOLVE SPLITS
-    ############################################################
-
-    shoe1.splcheck()
-    nose = []
-
-    while shoe1.spavailable:
-
-        shoe1.spvloc = [x for x in shoe1.spvloc if x not in nose]
-        if shoe1.spvloc == []:
-            break
-
-        for i in shoe1.spvloc:
-            item = shoe1.cards[i]
-
-            spl = input(f"Would you like to split hand {i}: {item}? (y/n)")
-
-            if spl == 'y':
-                shoe1.split(i)
-                print(f'Your Cards Are {shoe1.cards}')
-            elif spl == 'n':
-                nose.append(i)
+    
+    if das == False:
+        while True:
+            dbl = input(f'Double on hand {0}: {action.cards[0]} (y/n)')
+            if dbl.lower() == 'y':
+                dbl = True
+                break
+            elif dbl.lower() == 'n':
+                dbl = False
+                break
             else:
-                print("Error, Return (y/n)")
+                print("Please enter y or n")
 
-        shoe1.splcheck()
-
-    ############################################################
-    # DOUBLES
-    ############################################################
-
-    bustcounter = 0
-
-    for i in range(len(shoe1.cards)):
-        item = shoe1.cards[i]
-        dbl = input(f"Would you like to double on hand {i}: {item}? (y/n)")
-
-        if dbl == "y":
-            print("Your bet has been doubled!")
-            shoe1.double(i)
-            print(f"Your Hand is {shoe1.cards[i]}")
-            shoe1.valchk(i)
-
-            if shoe1.value > 21:
-                print("BUST")
-
-                stack.lose()
-                stack.lose()
-
-                del shoe1.cards[i]
-                bustcounter += 1
-
-        elif dbl == "n":
-            continue
-
-        else:
-            print("Please Enter (y/n)")
+        player.preseq(handid = 0, betid = 0, dbl = dbl, spl = False, das = das, spa = spa, rsa = rsa, maxsplits = maxsplits)
 
 
-    if bustcounter >= len(shoe1.cards):
-        continue
 
-    ############################################################
-    # HIT OR STAND
-    ############################################################
-
-    for i in range(len(shoe1.cards)):
-        item = shoe1.cards[i]
-
-        if i not in shoe1.doublist:
-
-            while 1 == 1:
-                ans = input(f"Hit or Stand on hand {i}: {item}")
-                if ans.lower() == "hit":
-                    shoe1.hit(i)
-                    print(f'Your Cards Are {shoe1.cards}')
-                    shoe1.valchk(i)
-                    if shoe1.value > 21:
-                        print("BUST (rip bozo)")     
-                        stack.lose()
-                        del shoe1.cards[i]
-                        bustcounter += 1
+    if (das == False and dbl == False) or das == True:
+        n = 0
+        while n < len(action.cards):
+            if action.cards[n][0] == action.cards[n][1] and player.acesplit_prohibiting == False and (maxsplits is None or action.splitcount < maxsplits):
+                while True:
+                    spl = input(f'Split on hand {n}: {action.cards[n]}? (y/n)')
+                    if spl.lower() == 'y':
+                        spl = True
                         break
-                elif ans.lower() == "stand":
-                    shoe1.valchk(i)        
+                    elif spl.lower() == 'n':
+                        spl = False
+                        break
+                    else:
+                        print("Please enter y or n")
+
+                player.preseq(handid = n, betid = n, dbl = False, spl = spl, das = das, spa = spa, rsa = rsa, maxsplits = maxsplits)
+                print(f'Your new hands are {action.cards}')
+                if spl == True:
+                    continue
+                else:
+                    n += 1
+            else:
+                n += 1
+        
+        if action.splitcount == maxsplits:
+            print('Maximum split amount reached')
+
+            
+
+    if das == True:
+        for i, hand in enumerate(action.cards):
+            while True:
+                dbl = input(f'Double on hand {i}: {action.cards[i]}? (y/n)')
+                if dbl.lower() == 'y':
+                    dbl = True
+                    break
+                elif dbl.lower() == 'n':
+                    dbl = False
                     break
                 else:
-                    print("Please Enter Either Hit or Stand.")
+                    print("Please enter y or n")
 
-    if bustcounter >= len(shoe1.cards):
-        continue
+            player.preseq(handid = i, betid = i, dbl = dbl, spl = False, das = das, spa = spa, rsa = rsa, maxsplits = maxsplits)
 
-    ############################################################
-    # DEALER PLAY
-    ############################################################
 
-    print(f'Dealer shows {shoe1.dlist}')
 
-    shoe1.dchk()
+    for i, hand in enumerate(action.cards):
+        if i not in stack.doubles:
+            while 6 != 7:
+                yit = input(f'Hit or Stand on hand {i}: {action.cards[i]}?')
+                if yit.lower() in ['hit', 'h']:
+                    action.hit(i)
+                    player.handval(hand)
+                    if player.value > 21:
+                        break
+                    else:
+                        continue
+                elif yit.lower() in ['stand','s']:
+                    break
+                else:
+                    print("Please enter hit (h) or stand (s)")
 
-    print(f'Dealer ends with {shoe1.dlist}')
 
-    ############################################################
-    # SETTLE WINS AND LOSSES
-    ############################################################
 
-    if shoe1.dvalue > 21:
-        print("Dealer Busts!")
-        for i in range(len(shoe1.cards)):
-            stack.win()
-            if i in shoe1.doublist:
-                stack.win()
-                print("Nice Double!")
-        print(f"You now have {stack.size} chips")
-    else:
-        for i in range(len(shoe1.cards)):
-            item = shoe1.cards[i]
-            shoe1.valchk(i)
-            if shoe1.value > shoe1.dvalue:
-                print(f"You win with {item}")
-                stack.win()
-                if i in shoe1.doublist:
-                    stack.win()
-                    print("Nice Double!")
-            elif shoe1.value == shoe1.dvalue:
-                print(f"Push with {item}")
-            else:
-                print(f"You lose with {item}")
-                stack.lose()
-                if i in shoe1.doublist:
-                    stack.lose()
-                    print("Bad Double!")
+    for i, hand in enumerate(action.cards):
+        res = game.endhand(i, h17)
+        print(f'Hand {i} ends on {action.cards[i]}, {stack.bets[i]} wagered')
+        if res == 'bust':
+            print(f'You Busted! good boy.')
+        else:
+            print(f'Dealer shows {action.dealerhand[1]}')
+            for i in range((len(action.dealerhand) - 2)):
+                print(f'Dealer draws {action.dealerhand[i+2]}')
+            print(f'Dealer ends on {action.dealerhand}')
+            if res == 'dbust':
+                print(f'The Dealer Busts! Nice!')
+            elif res == 'lose':
+                print(f'You Lose')
+            elif res == 'push':
+                print('Push')
+            elif res == 'win':
+                print('You Win!')
+
+
+
+
+
+
+    if len(shoe.order)/(d*52) < pen:
+        shoe.reset()
